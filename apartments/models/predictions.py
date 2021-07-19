1a97d91b6f8d2ddb972d2af9b953d4acf13eba9a
sprifti <serenaprifti5@gmail.com>
2021-07-19 23:12:02 +0200

    up to 2 decimals

--- apartments/models/predictions.py
+++ apartments/models/predictions.py
@@ -5,7 +5,7 @@ class Predictions(models.Model):
     apartment = models.ForeignKey(
         to="Apartment", null=True, on_delete=models.SET_NULL
     )
-    predicted_price = models.DecimalField(max_digits=40, decimal_places=20, null=False)
+    predicted_price = models.DecimalField(max_digits=40, decimal_places=2, null=False)
     accurate_price = models.BooleanField(null=True)
 
     class Meta:
